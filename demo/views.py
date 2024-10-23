5627996cfcc138d8ac18019a3d0787701a9b43b2
SakariaNdadi <oipapi.ndadi@gmail.com>
2024-10-23 16:07:02 +0200

    "Updated views.py to use list of dictionaries for car data and modified index.html to comment out various UI components and add a table component"

--- demo/views.py
+++ demo/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Car
+from django.views.generic import ListView
+from django.views.generic.list import MultipleObjectMixin
 
 
 # Create your views here.
 def index(request):
     cars = Car.objects.all()
-    context = {"cars": cars}
+    # context = {"cars": cars}
+    context = {"cars": list(cars.values())}
     return render(request, "index.html", context)
