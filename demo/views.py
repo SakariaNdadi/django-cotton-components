f5574f519bc658a9a6319a230edecfab17314c02
SakariaNdadi <oipapi.ndadi@gmail.com>
2024-10-23 19:01:17 +0200

    "Updated views.py to pass additional context to index.html, and modified various templates (h2, h3, h4, password_input, radio, select_multiple, table) to add attributes and classes. Also, reorganized and refactored

--- demo/views.py
+++ demo/views.py
@@ -7,6 +7,5 @@ from django.views.generic.list import MultipleObjectMixin
 # Create your views here.
 def index(request):
     cars = Car.objects.all()
-    # context = {"cars": cars}
-    context = {"cars": list(cars.values())}
+    context = {"cars": cars, "table_cars": list(cars.values())}
     return render(request, "index.html", context)
